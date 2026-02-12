9f1c6719d3bcf478b85e8e056517cd44acc3a7bb
drumcirclepakistan <drumcirclepakistan@gmail.com>
2026-02-12 13:09:56 +0500

    Add trust proxy setting to Express app

--- server/index.ts
+++ server/index.ts
@@ -4,6 +4,7 @@ import { serveStatic } from "./static";
 import { createServer } from "http";
 
 const app = express();
+app.set("trust proxy", 1); // <--- Add this line
 const httpServer = createServer(app);
 
 declare module "http" {
